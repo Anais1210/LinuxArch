6c0a8cd9e2ce606d9f650337e0289d1750234221
Anais1210 <azhang2@myges.fr>
2022-04-17 13:40:25 +0200

    all commands

--- linuxArch.sh
+++ linuxArch.sh
@@ -13,3 +13,17 @@ pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 timedatectl set-timezone Europe/Paris
+echo [your_hostname] > /etc/hostname
+touch /etc/hosts
+systemctl enable dhcpcd
+passwd
+pacman -S grub os-prober
+grub-install /dev/sdX
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S grub efibootmgr
+mkdir /boot/efi
+mount /dev/sdX1 /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+sudo reboot
